e5cbda1ffc1e5839251c98b6190e015d15796b48
nwheeler <nwheeler48@gmail.com>
2019-02-27 19:36:25 -0500

    Intial Commit to repository. Contains the app and unit tests, also started to work on some of the data model stuff and created a http get method to retrieve a buffer from the enpoint.

---
+++ MLBSeasonResults/Model/Utility/EndpointDataUtility.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.Web.Http;
+
+
+
+namespace MLBSeasonResults.Model.Utility
+{
+    /// <summary>
+    /// Class: EndpointDataUtility
+    /// Purpose: Retrieve data from a specified endpoint and make it avaliable to other classes.
+    /// </summary>
+    public static class EndpointDataUtility
+    {
+        /// <summary>
+        /// Retrieve data from the specified endpoint.
+        /// </summary>
+        /// <param name="address">The full address of the endpoint which contains the data</param>
+        /// <returns></returns>
+        public static async Task<IBuffer> GetDataBuffer(string address)
+        {
+
+            var uri = new System.Uri(address);
+
+            // Attempt to get the data from the provided enpoint.
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var response = await httpClient.GetAsync(uri);
+
+                    // Check that the response was correctly carried out.
+                    if (response.StatusCode == HttpStatusCode.Ok)
+                    {
+                        // If the call completed sucessfully return the buffer.
+                        return await response.Content.ReadAsBufferAsync();
+                    }
+                    else
+                    {
+                        // If the http request failed throw an exception that will need to be caught be the caller.
+                        throw new Exception(response.StatusCode.ToString());
+                    }
+                    
+                }
+                catch (COMException e)
+                {
+                    // TODO: How to handle COMException.
+                    throw e;
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new ArgumentNullException("A null enpoint was inappropriately provided.");
+                }
+                catch (OutOfMemoryException)
+                {
+                    throw new OutOfMemoryException("The host system has run out of memory and cannot complete the operation.");
+                }
+            }
+        }
+    }
+}
---
+++ MLBSeasonResults/Model/Utility/TeamSeasonResults.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLBSeasonResults.Model.Utility
+{
+    public class TeamSeasonResults : INotifyPropertyChanged
+    {
+        #region Fields
+        #endregion
+
+        #region Properties
+        private string _teamName;
+        public string TeamName
+        {
+            get
+            {
+                return _teamName;
+            }
+            set
+            {
+                if (value != null)
+                {
+                    _teamName = value;
+                    OnPropertyChanged("TeamName");
+                }
+            }
+        }
+
+        private string _wins;
+        public string Wins
+        {
+            get { return _wins; }
+            set
+            {
+                if (value != null)
+                {
+                    _wins = value;
+                    OnPropertyChanged("Wins");
+                }
+            }
+        }
+
+        private string _losses;
+        public string Losses
+        {
+            get { return _losses; }
+            set
+            {
+                if (value != null)
+                {
+                    _losses = value;
+                    OnPropertyChanged("Losses");
+                }
+            }
+        }
+            
+        #endregion
+
+        #region Constructors
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+    }
+}
